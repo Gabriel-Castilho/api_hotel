12d8519c53bff27c453e4a4b4c909fc223009f89
Gabriel Alexandre <gabriel.castilho@outlook.com.br>
2021-08-24 13:20:29 -0300

    remove cors

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const routes = require("./src/routes/index")
-const cors = require("cors")
+//const cors = require("cors")
 const app = express();
 
 
-app.use(cors())
+//app.use(cors())
 /*app.use(cors({
   origin: "https://applicationdatecontrol.herokuapp.com",
 })
