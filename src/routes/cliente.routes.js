52cc13f67f7bb9b2c75732cf4c8f407bfe552730
Gabriel <gabriel.castilho@outlook.com.br>
2021-10-30 13:29:15 -0300

    add status code

--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -32,10 +32,11 @@ clienteRouter.delete("/:cpf", async (req, res) => {
 clienteRouter.get("/:cpf", async (req, res) => {
     const { cpf } = req.params
     const items = await clienteController.getId(cpf)
-    if (items != []) {
-        return res.json(items)
+    if (items == "") {
+        res.sendStatus(404)
     } else {
-        res.status(404).json('Not Found')
+      
+        return res.json(items)
     }
 })
 
