f37542bf4b40fac20993aa69d7332d0201dabc48
Gabriel <gabriel.castilho@outlook.com.br>
2021-10-30 13:22:54 -0300

    add status code

--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -1,51 +1,49 @@
-const {Router} = require("express")
+const { Router } = require("express")
 const ClienteController = require("../controllers/ClienteController")
 
 const clienteRouter = Router();
 const clienteController = new ClienteController();
 
-clienteRouter.get("/",async (req,res)=>{
+clienteRouter.get("/", async (req, res) => {
     const items = await clienteController.index()
-    if(items != undefined || items != '' || items != []){
+    if (items != undefined || items != '' || items != []) {
         return res.json(items)
-    }else{
+    } else {
         res.status(404)
     }
 })
 
-clienteRouter.post("/",async(req,res)=>{
-    const{nome,sobrenome,telefone,cpf} = req.body
-    const items = await clienteController.create(nome,sobrenome,telefone,cpf)
+clienteRouter.post("/", async (req, res) => {
+    const { nome, sobrenome, telefone, cpf } = req.body
+    const items = await clienteController.create(nome, sobrenome, telefone, cpf)
     return res.json(items)
 })
 
-clienteRouter.delete("/:cpf",async(req,res)=>{
-    const {cpf} = req.params
+clienteRouter.delete("/:cpf", async (req, res) => {
+    const { cpf } = req.params
     const items = await clienteController.delete(cpf)
-    if(items != undefined || items != '' || items != []){
+    if (items != undefined || items != '' || items != []) {
         return res.json(items)
-    }else{
+    } else {
         res.sendStatus(404)
     }
 })
 
-clienteRouter.get("/:cpf",async(req,res)=>{
-    const {cpf}= req.params
+clienteRouter.get("/:cpf", async (req, res) => {
+    const { cpf } = req.params
     const items = await clienteController.getId(cpf)
-    if( items != ''){
-         res.status(200)
+    if (items != '') {
         return res.json(items)
-
-    }else{
+    } else {
         return res.status(404)
     }
 })
 
-clienteRouter.patch("/:cpf",async(req,res)=>{
-    const {cpf} = req.params
-    const {nome,sobrenome,telefone} = req.body
-    const items = await clienteController.update(nome,sobrenome,telefone,cpf)
+clienteRouter.patch("/:cpf", async (req, res) => {
+    const { cpf } = req.params
+    const { nome, sobrenome, telefone } = req.body
+    const items = await clienteController.update(nome, sobrenome, telefone, cpf)
     return res.json(items)
 })
 
-module.exports=clienteRouter
+module.exports = clienteRouter
