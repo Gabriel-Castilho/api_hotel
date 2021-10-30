35c66714b462a9d86743043d748abab5b24ffc7c
Gabriel <gabriel.castilho@outlook.com.br>
2021-10-30 19:09:33 -0300

    .

--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -13,6 +13,7 @@ clienteRouter.post("/", async (req, res) => {
     const { nome, sobrenome, telefone, cpf } = req.body
     const items = await clienteController.create(nome, sobrenome, telefone, cpf)
     if(items =='message:"erro"'){
+        console.log('error')
         res.status(422).json('cpf já cadastrado')
     }
     return res.json(items)
